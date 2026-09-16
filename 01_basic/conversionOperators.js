8436fa5d04fb414f6bc362273a94509ed9c77761
Ahmad Anwar <ahmadanwar3035@gmail.com>
2026-09-16 07:57:37 +0000

    conversionOperation

---
+++ 01_basic/conversionOperators.js
@@ -0,0 +1,55 @@
+
+
+// let score = 33;
+// console.log( typeof score);   
+// // ---> typye of number 
+
+// let score1 = "33";
+// console.log(typeof score1);
+
+// // convert string intio number use number method ()
+
+// let convertNumberValue = Number(score1)
+// console.log( typeof convertNumberValue);
+
+
+// " 33" convert in number (33)  ayh easily change ho jata h 
+// "33abc "  convert in number ? ==> NaN
+// true  convert ni number --> (1)
+// null convert in number give output --> 0
+
+
+// convert  in number ,convert in string ,convert in boolean
+
+// convert in number
+//  let scrore = "3345";
+//  console.log( typeof scrore);
+ 
+//  let convertInNumber = Number(scrore)
+//  console.log( typeof convertInNumber);
+  
+
+
+
+//  conver in string 
+// let  age = 23;
+// console.log( typeof age);
+
+// let convertInString = String(age)
+// console.log( typeof convertInString);
+
+
+// conver in boolean
+// let logIN = "true";
+// console.log( typeof logIN);
+
+// let contvetInBoolean = Boolean(logIN)
+// console.log(contvetInBoolean);
+
+
+// convert  empty ""
+//  let logIN = "";
+// console.log( typeof logIN);
+
+// let contvetInBoolean = Boolean(logIN)
+// console.log(contvetInBoolean);
--- 01_basic/test.js
+++ 01_basic/test.js
@@ -3,3 +3,5 @@
 // console.log("hello javascript")
 
 // console.log("javascript");
+
+// console.log("ahmad")
