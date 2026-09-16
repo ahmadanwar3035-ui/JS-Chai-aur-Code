d7b955d18b4554391a8ce8cd45f4a097f45f7958
Ahmad Anwar <ahmadanwar3035@gmail.com>
2026-09-16 11:48:49 +0000

    heao and stack

--- 01_basic/05_datatypye_summary.js
+++ 01_basic/05_datatypye_summary.js
@@ -34,3 +34,56 @@ const myFunction = function(){
     
 }
 myFunction()
+
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Memory are two type ..>
+// 1==> steack menory  (primitive datatype main stack memory use hoti h)
+// 2==> heap memory (non- primitive datatype main heap  memory use hoti h)
+
+
+// Stack memory ...jab b use krty h variable   bany k liya tu hum ko ik copy  milta h ... 
+
+// Heap memory ....jab  b hum non - primitve datatype sa variable bany ag ty ..hum ko ik refernce mily ga original copy ka
+//  or jo b value change kry vo original  value amin b change joag
+let myYoutubeName = "ahmadanwar";
+// console.log(myYoutubeName);
+
+let anotherYoutubeChannel = myYoutubeName
+anotherYoutubeChannel = "ahmad"
+// console.log(anotherYoutubeChannel);
+// console.log(myYoutubeName);
+
+
+let user = {
+  name: "Ahmad",
+  age: 20,
+  city: "Lahore"
+};
+
+// let product = user=  {
+//   name: "iPhone",
+//   price: 150000,
+//   brand: "Apple"
+// };
+
+let userTwo = user 
+    userTwo.name = "ali"
+    // console.log(userTwo);
+    // console.log(user);
+    
+    
+
+
+
+
+
+
+
+
+
+
+
