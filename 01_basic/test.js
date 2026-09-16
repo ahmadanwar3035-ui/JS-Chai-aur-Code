7789dc59c5d6a712d3b629398b388e02b9f37e56
Ahmad Anwar <ahmadanwar3035@gmail.com>
2026-09-16 09:09:56 +0000

    test  probems solve

--- 01_basic/test.js
+++ 01_basic/test.js
@@ -5,3 +5,54 @@
 // console.log("javascript");
 
 // console.log("ahmad")
+
+
+// Problem 1
+let age = "25";
+// Is age ko Number mein convert karo aur us mein 5 add karke result console mein print karo.
+
+let convertAge = Number(age)
+convertAge = convertAge+5
+// console.log(convertAge);
+
+
+// Problem 2
+let price = 100;
+let quantity = "3";
+// Dono values ko use karke total price = 300 console mein print karo.
+let converQunatity = Number(quantity)
+// console.log(converQunatity);
+let totalQunatity = price * converQunatity
+// console.log(totalQunatity);
+
+
+
+// Problem 3
+let num1 = "20";
+let num2 = 10;
+// Aisa code likho jo dono ko Number bana kar unka sum print kare.
+let convetNUmberInValue = Number(num1)
+// console.log(convetNUmberInValue);
+
+let totalSumPrice = convetNUmberInValue +  num2
+// console.log(totalSumPrice);
+
+
+
+
+// Problem 4
+let firstName = "Ahmad";
+let age1 = 20;
+let isStudent = true;
+// Har variable ka data type console mein check karo.
+// console.log(typeof firstName );
+// console.log(typeof age1  );
+// console.log(typeof  isStudent);
+
+
+
+// Problem 5
+let marks = "80";
+// marks ko Number mein convert karo aur check karo:
+let  convertMarkValue =Number(marks)
+// console.log(convertMarkValue);
