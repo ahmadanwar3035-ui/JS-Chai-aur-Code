2a628d05ee19d3b44ff99e2dc15c305dac0be69d
Ahmad Anwar <ahmadanwar3035@gmail.com>
2026-09-17 10:39:12 +0000

    data and time

---
+++ 01_basic/07_nums_maths.js
@@ -0,0 +1,25 @@
+/// first number variavle deaclare krny ..
+
+const score = 300
+// console.log(score);
+
+// second number varoablle deaclare krny 
+const balance = new Number(30000)
+// console.log(balance.toString());
+//  console.log(balance.toFixed(2));
+ 
+// +++++++++++++++++++++++++++++++++++++++++++++ Maths+++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);   yah ik object h 
+
+// console.log(Math.abs(-10));   neagative value posttive main convert krta h 
+// console.log(Math.round(32.9));  roundoff krta h vlaue ko integer  main
+// console.log(Math.min(3,4,5,6,7,8,9));  sab sa choti value  lo
+// console.log(Math.max(23,45,56567,78,6543,98765432));
+// console.log(Math.random());   zero sa one tak  k  beach value ay gi
+
+
+
+
+
+
